ba7e671e4b4a57dca96615085fe11ab8bed6d32e
Niharika Joshi <89737890+Niharikajo@users.noreply.github.com>
2023-01-03 15:43:20 +0530

    Add files via upload

---
+++ scripts/dataset4_Autoformer_M_96.sh
@@ -0,0 +1,17 @@
+python -u run.py \
+  --root_path ./dataset/ETT-small/ \
+  --data_path dataset4.csv \
+  --exp_name dataset4_Autoformer_M_96_96 \
+  --model Autoformer \
+  --data custom \
+  --features M \
+  --seq_len 96 \
+  --label_len 48 \
+  --pred_len 96 \
+  --e_layers 2 \
+  --d_layers 1 \
+  --factor 1 \
+  --enc_in 3 \
+  --dec_in 3 \
+  --c_out 3
+
---
+++ scripts/dataset4_Transformer+EMA_M_96.sh
@@ -0,0 +1,21 @@
+python -u run.py \
+  --root_path ./dataset/ETT-small/ \
+  --data_path dataset4.csv \
+  --exp_name dataset4_Transformer+EMA_M_96_96 \
+  --model Transformer \
+  --data custom \
+  --features M \
+  --seq_len 96 \
+  --label_len 48 \
+  --pred_len 96 \
+  --e_layers 2 \
+  --d_layers 1 \
+  --factor 1 \
+  --enc_in 3 \
+  --dec_in 3 \
+  --c_out 3\
+  --use_ema \
+  --epsilon 0.001 \
+  --lr 0.0003 \
+  --lradj no
+
